b01d5515538df109e450d27015d1b219ab9d89b2
ivxxi <crystalalice21@gmail.com>
2020-07-15 01:35:17 +0300

    add

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -11,14 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 import dj_database_url
 from decouple import config,Csv
-
-
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
@@ -48,10 +42,17 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY =config('SECRET_KEY')
 
 DEBUG = True
 
@@ -64,7 +65,7 @@ DEBUG =config('DEBUG',default=False,cast=bool)
 
 LOGIN_REDIRECT_URL = ('/')
 
-SECRET_KEY = 'fcubj^3r(ky&@g!q%9t4(v4cgdx201882%@b8q4im(8a2(_+wy'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -176,6 +177,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Configure Django App for Heroku.
-django_heroku.settings(locals())
