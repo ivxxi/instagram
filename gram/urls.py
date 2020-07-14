a13c911ec3867635b785ef49d7e2f12567df4f1e
ivxxi <crystalalice21@gmail.com>
2020-07-15 00:58:22 +0300

    commit

---
+++ gram/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.index,name='index'),
+    url(r'^$', views.homepage,name='home'),
+    url(r'^profiles/(?P<username>[-_\w.]+)$', views.profile,name='profiles'),
+    url(r'^profiles/post/$', views.posts,name='uploadpost'),
+    url(r'^profiles/edit/(?P<username>[-_\w.]+)$', views.update_profile,name='editprofile'),
+    url(r'^update-profile-picture/(?P<username>[-_\w.]+)$', views.profile_pic_update, name='update_profilepic'),
+    url(r'^profiles/upvote/(\d+)$', views.upvote_posts, name="upvote_post"),
+    url(r'^profiles/comment/(\d+)$', views.comment, name="comment"),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
