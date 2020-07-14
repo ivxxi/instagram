8a2d97c584ae1af47d01623af5d49c7b0dfcf0cb
ivxxi <crystalalice21@gmail.com>
2020-07-15 01:22:07 +0300

    add

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -51,8 +51,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =config('SECRET_KEY')
+
 
 DEBUG = True
 
