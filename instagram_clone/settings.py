c9bb053b008a538f5210d449551952ddaad82d82
ivxxi <crystalalice21@gmail.com>
2020-07-15 02:17:13 +0300

    deploy

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -56,7 +56,7 @@ SECRET_KEY =config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG',default=False,cast=bool)
